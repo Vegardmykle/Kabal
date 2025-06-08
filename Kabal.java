7b4577e6733658fda85a03728ff621425f082a1d
Vegardmykle <gass_genera_04@icloud.com>
2025-06-08 14:41:01 +0200

    legger til en bunke med de resterende kortene

--- Kabal.java
+++ Kabal.java
@@ -1,31 +1,35 @@
-public class Kabal{
-    
-    public Kort[] rad(KortStokk s,int rNr){
+public final class Kabal{
+    private KortStokk kortStokk = new KortStokk(); 
+    private Kort[][] start = start(kortStokk);
+    private Kort[] bunke;
+    public Kabal(){
+        skjulK();
+        bunke = trekkKortBunke();
+    }
+    public Kort[] rad(int rNr){
         Kort rad[] = new Kort[rNr];
         for (int i = 0; i < rNr; i++) {
-            rad[i] = s.trekkKort();
+            rad[i] = kortStokk.trekkKort();
         }
 
 
         return rad;
     }
+    //skal bare kalles en gang
     public Kort[][] start(KortStokk s){
         Kort spill[][] = new Kort[7][];
         for (int i = 0; i < 7; i++) {
-            spill[i] = rad(s, i+1);
+            spill[i] = rad(i+1);
         }
 
         return spill;
     }
 
-    public void skjulK(KortStokk s){
-        Kort[][] start = start(s);
+    public void skjulK(){
         for(int i = 0; i<start.length;i++){
             for (int z = 0;z<start[i].length;z++){
-                if(start[i].length-1!=z){
+                if(start[i].length-1!=z)
                     System.out.print(start[i][z].getBakside() +" ");
-
-                }
                 else
                     System.out.print(start[i][z].toString() +" ");
                 
@@ -34,12 +38,25 @@ public class Kabal{
         }
     }
 
+    // må skje etter at kabal er lagt
+    public Kort[] trekkKortBunke(){
+        Kort[] bunke = new Kort[24];
+        for(int i =0; i<24;i++){
+            bunke[i] = kortStokk.trekkKort();
+        }
+        return bunke;
+    }
+    public Kort[] hentBunke(){
+        return bunke;
+    }
+
+    
+
     public static void main(String[] args) {
-        KortStokk k = new KortStokk();
-        Kabal kabal = new Kabal();
-
-        kabal.skjulK(k);
-
+        Kabal k = new Kabal();
+        for (Kort kort : k.hentBunke()) {
+            System.out.println(kort.toString());
+        }
     }
        
 }
