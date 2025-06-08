e97862ec2ad9f16ecdebf5e8213954700a4925fe
Vegardmykle <gass_genera_04@icloud.com>
2025-06-08 14:20:14 +0200

    skjuling av kort bak

--- Kabal.java
+++ Kabal.java
@@ -9,25 +9,36 @@ public class Kabal{
 
         return rad;
     }
-    public Kort[][] spill(KortStokk s){
+    public Kort[][] start(KortStokk s){
         Kort spill[][] = new Kort[7][];
         for (int i = 0; i < 7; i++) {
             spill[i] = rad(s, i+1);
         }
+
         return spill;
     }
-    
+
+    public void skjulK(KortStokk s){
+        Kort[][] start = start(s);
+        for(int i = 0; i<start.length;i++){
+            for (int z = 0;z<start[i].length;z++){
+                if(start[i].length-1!=z){
+                    System.out.print(start[i][z].getBakside() +" ");
+
+                }
+                else
+                    System.out.print(start[i][z].toString() +" ");
+                
+            }
+            System.out.println("\n");
+        }
+    }
+
     public static void main(String[] args) {
         KortStokk k = new KortStokk();
         Kabal kabal = new Kabal();
 
-        Kort s[][] = kabal.spill(k);
-        for(Kort[] kort : s){
-            for (Kort m : kort){
-                System.out.print(m.toString() +" ");
-            }
-            System.out.println("\n");
-        }
+        kabal.skjulK(k);
 
     }
        
